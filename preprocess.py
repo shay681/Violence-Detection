ffb60f3873b4493bd5fb594ab153f99bd2d4fa9b
shay681 <shay681@gmail.com>
2022-12-25 18:24:23 +0200

    delete comments

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,3 @@
-# from utils import *
-
-# VIOLENT_PATH = 'data/train/Fight/'
-# NON_VIOLENT_PATH = 'data/train/NonFight'
-
-# if __name__ == "__main__":
-#     violent_videos_files = get_file_names_from_dir(VIOLENT_PATH)
-#     non_violent_videos_files = get_file_names_from_dir(NON_VIOLENT_PATH)
-
-#     video = read_video_from_file(violent_videos_files[-1])
-#     video_frames = break_video_into_frames(video, True)
-#     loop_frames(video_frames)
-
 import cv2
 import numpy as np
 import os
